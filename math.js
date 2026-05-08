8dadec6387e6fa7672f62e120c45aeb9b70e8166
Nayana Deepthy R <nayanadeepthyr@gmail.com>
2026-05-08 23:25:12 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,51 @@
+// // const sum=(a,b)=> a+b;
+// // const multiply =(a,b)=>a*b;
+// // const g= 9.64;
+// // const pi =3.1432;
+// // module.exports  =123;
+// // exporting objects 
+// // module.exports = {sum,mutliply,g,pi};
+// // let obj = {
+// //     sum:sum,
+// //     multiply:multiply,
+// //     g:g,
+// //     pi:pi
+
+// // }
+// // module.exports = obj;
+// exports.sum = (a,b)=>a+b; // another way of exporting 
+
+// // using import 
+// export const sum=(a,b)=> a+b;
+// export const multiply =(a,b)=>a*b;
+// export const g= 9.64;
+// export const pi =3.1432;
+
+
+
+
+// const sum = (a,b)=> a+b;
+// const mul = (a,b)=> a*b;
+// const g = 9.8;
+// const pi = 3.14;
+// // module.exports = 123;
+
+// let obj = {
+//     mul:mul,
+//     sum:sum,
+//     g:g,
+//     pi:pi,
+// };
+
+// module.exports = obj;
+// using import 
+export const sum = (a,b)=> a+b;
+export const mul = (a,b)=> a*b;
+export const div = (a,b)=> a/b;
+export const modulo = (a,b)=> a%b;
+export const g = 9.8;
+export const pi = 3.1432;
+
+
+
+
